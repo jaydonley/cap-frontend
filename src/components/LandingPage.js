f3231a20580d865e4a83cec542d8fcfc15c27d57
Jason Donley <jay.donley@gmail.com>
2022-12-14 11:48:57 -0500

    updatedBottomNav

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -142,18 +142,7 @@ class LandingPage extends Component {
   <div class="container">
     <div class="navbar-header w-100">
       <nav class="navbar navbar-expand-lg navbar-dark px-0">
-        <Link class="navbar-brand logo" href="/">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Seal_of_the_City_of_Cincinnati_%28Ohio%29.png/1200px-Seal_of_the_City_of_Cincinnati_%28Ohio%29.png"  id="logoSize"  alt="Seal of Cincinnati" height={100}/>
-          </Link>
-          
-          
-        
-  
-        <button style={{color: 'darkblue'}} class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar01" 
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span  style={{color: 'darkblue'}}  class="navbar-toggler-icon"></span>
-        </button>
-  
+     
         <div class="collapse navbar-collapse" id="navbar01">
           <ul class="navbar-nav navigation-menu ml-auto">
             <li class="nav-item" >
