a2bea692102320dad6823b079be382befbe6b965
Jason Donley <jay.donley@gmail.com>
2022-12-14 15:09:57 -0500

    v5

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -70,16 +70,16 @@ function navbar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand  href="/"></Navbar.Brand>
+        <Navbar.Brand  to="/"></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/CultureHistory">Culture & History</Nav.Link>
-            <Nav.Link href="/Attractions">Attractions</Nav.Link>
-            <Nav.Link href="/Sports">Sports Venues</Nav.Link>
-            <Nav.Link href="/Contact">Contact Us</Nav.Link>
-            <Nav.Link href="/teamPage">Our Team</Nav.Link>
+            <Nav.Link to="/">Home</Nav.Link>
+            <Nav.Link to="/CultureHistory">Culture & History</Nav.Link>
+            <Nav.Link to="/Attractions">Attractions</Nav.Link>
+            <Nav.Link to="/Sports">Sports Venues</Nav.Link>
+            <Nav.Link to="/Contact">Contact Us</Nav.Link>
+            <Nav.Link to="/teamPage">Our Team</Nav.Link>
             
              
           </Nav>
