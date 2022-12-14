50881b54360da98b9ab9b8d1d688f34e428f6dc3
Jason Donley <jay.donley@gmail.com>
2022-12-14 14:59:39 -0500

    navbarWork

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,55 +1,91 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
- 
-class navbar extends Component {
-    render() {
-        return (
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown'; 
 
-<header id="navigation" class="navigation">
-  <div class="container">
-    <div class="navbar-header w-100">
-      <nav class="navbar navbar-expand-lg navbar-dark px-0">
-        <Link class="navbar-brand logo" to="/">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Seal_of_the_City_of_Cincinnati_%28Ohio%29.png/1200px-Seal_of_the_City_of_Cincinnati_%28Ohio%29.png"  id="logoSize"  alt="Seal of Cincinnati" height={100}/>
-          </Link>
+
+
+// class navbar extends Component {
+//     render() {
+//         return (
+
+// <header id="navigation" class="navigation">
+//   <div class="container">
+//     <div class="navbar-header w-100">
+//       <nav class="navbar navbar-expand-lg navbar-dark px-0">
+//         <Link class="navbar-brand logo" to="/">
+//           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Seal_of_the_City_of_Cincinnati_%28Ohio%29.png/1200px-Seal_of_the_City_of_Cincinnati_%28Ohio%29.png"  id="logoSize"  alt="Seal of Cincinnati" height={100}/>
+//           </Link>
           
           
         
   
-        <button style={{color: 'darkblue'}} class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar01" 
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span  style={{color: 'darkblue'}}  class="navbar-toggler-icon"></span>
-        </button>
+//         <button style={{color: 'darkblue'}} class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar01" 
+//           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//           <span  style={{color: 'darkblue'}}  class="navbar-toggler-icon"></span>
+//         </button>
   
-        <div class="collapse navbar-collapse" id="navbar01">
-          <ul class="navbar-nav navigation-menu ml-auto">
-            <li class="nav-item" >
-              <Link style={{color: '#262629'}} class="nav-link" to="/">Home</Link>
-            </li>
+//         <div class="collapse navbar-collapse" id="navbar01">
+//           <ul class="navbar-nav navigation-menu ml-auto">
+//             <li class="nav-item" >
+//               <Link style={{color: '#262629'}} class="nav-link" to="/">Home</Link>
+//             </li>
 
-            <li class="nav-item">
-              <Link style={{color: '#262629'}} class="nav-link" to="/CultureHistory">Culture & History</Link>
-            </li>
-            <li class="nav-item">
-              <Link style={{color: '#262629'}} class="nav-link" to="/Attractions">Attractions</Link>
-            </li>
-            <li class="nav-item">
-              <Link style={{color: '#262629'}} class="nav-link" to="/Sports">Sport Venues</Link>
-            </li>
-            <li class="nav-item">
-              <Link style={{color: '#262629'}} class="nav-link" to="/Contact">Contact</Link>
-            </li>
-            <li class="nav-item">
-              <Link style={{color: '#262629'}} class="nav-link" to="/teamPage">Team</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  </div>
-  </header>
+//             <li class="nav-item">
+//               <Link style={{color: '#262629'}} class="nav-link" to="/CultureHistory">Culture & History</Link>
+//             </li>
+//             <li class="nav-item">
+//               <Link style={{color: '#262629'}} class="nav-link" to="/Attractions">Attractions</Link>
+//             </li>
+//             <li class="nav-item">
+//               <Link style={{color: '#262629'}} class="nav-link" to="/Sports">Sport Venues</Link>
+//             </li>
+//             <li class="nav-item">
+//               <Link style={{color: '#262629'}} class="nav-link" to="/Contact">Contact</Link>
+//             </li>
+//             <li class="nav-item">
+//               <Link style={{color: '#262629'}} class="nav-link" to="/teamPage">Team</Link>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+//     </div>
+//   </div>
+//   </header>
 
-            );
-    }
+//             );
+//     }
+// }
+// export default navbar;
+
+
+
+
+
+
+
+function navbar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link1</Nav.Link>
+            <Nav.Link href="#link">Link2</Nav.Link>
+            <Nav.Link href="#link">Link3</Nav.Link>
+            <Nav.Link href="#link">Link4</Nav.Link>
+            
+             
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
 export default navbar;
